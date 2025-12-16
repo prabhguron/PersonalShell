9653b53903d0ac8aaa63cb341d059a9042665146
prabhguron <prabhten@gmail.com>
2025-12-16 15:27:54 -0800

    commnad not found

--- lib/main.ex
+++ lib/main.ex
@@ -2,5 +2,13 @@ defmodule CLI do
   def main(_args) do
     # TODO: Uncomment the code below to pass the first stage
      IO.write("$ ")
+    user_input = IO.gets("") |> String.trim()
+    IO.puts("#{user_input}: command not found ")
+
+
+
+
+     
+
   end
 end
