9e40e8c62b6f83bf2c8bb25ad80f2d49484c4ea8
prabhguron <prabhten@gmail.com>
2025-12-16 16:00:43 -0800

    shows $ first then the rest

--- lib/main.ex
+++ lib/main.ex
@@ -3,7 +3,8 @@
 defmodule CLI do
   def main(_args) do
     # TODO: Uncomment the code below to pass the first stage
-
+        
+      IO.write("$ ")
     Recursion.print_multiple_times("user_input", 1)
 
   end
