75dd34a6102962fda27019ae8492b01e9c2398d8
prabhguron <prabhten@gmail.com>
2025-12-18 17:09:02 -0800

    works?

--- lib/main.ex
+++ lib/main.ex
@@ -96,7 +96,9 @@ IO.puts(Enum.join(args, " "))
   file = System.find_executable(user_command)
 
   if file do 
-  {output, exit_code} = System.cmd(file, args)
+args_to = [user_command | args]
+
+  {output, exit_code} = System.cmd(file, args_to)
 
   IO.puts(output)
 
